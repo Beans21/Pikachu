b3f9695e3957a405b4ba8c7f0de2aa0199d78134
Beans21 <SKardashian19@windwardschool.org>
2016-09-07 09:33:50 -0700

    Pikachu Project1

---
+++ src/PikachuChatter.java
@@ -0,0 +1,18 @@
+
+public class PikachuChatter {
+
+	public static void main(String[] args) {
+		System.out.println("    Pika pika pika chu pika chu peeeee ka pika chu!");
+		Pikachu();
+		System.out.println();
+		System.out.println("    Pika? Pika pika pika chu peeeee ka chu!");
+		Pikachu();
+	}
+	
+	public static void Pikachu() {
+		System.out.println("(\\__/)");
+		System.out.println("(0^.^)");
+		System.out.println("z(_(\")(\")");
+	}
+	
+}
